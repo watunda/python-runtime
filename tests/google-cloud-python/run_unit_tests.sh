b5ff773291a10f4a5351e5cf3bb0cfca5a2486fb
Doug Greiman <dgreiman@google.com>
2017-12-20 15:51:02 -0800

    Add more information about test failures.

--- tests/google-cloud-python/run_unit_tests.sh
+++ tests/google-cloud-python/run_unit_tests.sh
@@ -5,6 +5,7 @@ cd /app/google-cloud-python
 
 exit_code=0
 for noxfile in */nox.py; do
+  echo "Running tests in ${noxfile}"
   nox \
     -f "${noxfile}" \
     -e \
@@ -12,7 +13,7 @@ for noxfile in */nox.py; do
     "unit(py='3.4')" \
     "unit(py='3.5')" \
     "unit(py='3.6')" \
-    || exit_code=1
+    || {echo "FAILED"; exit_code=1; }
 done
 
 exit "${exit_code}"
